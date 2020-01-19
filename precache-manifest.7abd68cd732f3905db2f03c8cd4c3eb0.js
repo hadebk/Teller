f6ce08bc869b7a892df4f82a9ba9cd5c27976b64
abdalhadi bakr <hade.bk.1998@gmail.com>
2020-01-19 19:24:19 +0200

    first commit

---
+++ precache-manifest.7abd68cd732f3905db2f03c8cd4c3eb0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47564f0ed16e83138d258471e8061a56",
+    "url": "/Teller/index.html"
+  },
+  {
+    "revision": "76ee8f7370839bda7175",
+    "url": "/Teller/static/css/2.a8e484ad.chunk.css"
+  },
+  {
+    "revision": "0e19151278f49731cdcb",
+    "url": "/Teller/static/css/main.6e024ffe.chunk.css"
+  },
+  {
+    "revision": "76ee8f7370839bda7175",
+    "url": "/Teller/static/js/2.187ac8a0.chunk.js"
+  },
+  {
+    "revision": "606b8a018d5d09277c2c3a029df8032b",
+    "url": "/Teller/static/js/2.187ac8a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0e19151278f49731cdcb",
+    "url": "/Teller/static/js/main.744857a2.chunk.js"
+  },
+  {
+    "revision": "4f9e2d9ab23859caaf5b",
+    "url": "/Teller/static/js/runtime-main.e2ea3a5b.js"
+  },
+  {
+    "revision": "56c6eda833f918c883ed39996811f4ac",
+    "url": "/Teller/static/media/Logo1.56c6eda8.svg"
+  },
+  {
+    "revision": "31d099ffd961ead6dcf87df40cfc5a7e",
+    "url": "/Teller/static/media/cover-bk-mob.31d099ff.jpg"
+  },
+  {
+    "revision": "f3c536998f64beacda725416fed08c0a",
+    "url": "/Teller/static/media/github-sign.f3c53699.svg"
+  },
+  {
+    "revision": "1a3efb7efff37cdc2af6c7c4ad533969",
+    "url": "/Teller/static/media/joker.1a3efb7e.png"
+  },
+  {
+    "revision": "4540af56b91720dc21e337f2f30f5d35",
+    "url": "/Teller/static/media/linkedin-logo.4540af56.svg"
+  },
+  {
+    "revision": "3d2ea3941675adc5bfb228c2b88d062a",
+    "url": "/Teller/static/media/mdb_logo.3d2ea394.svg"
+  },
+  {
+    "revision": "ac3bbd14148f3122ed97d7a9227a0c1f",
+    "url": "/Teller/static/media/no-result.ac3bbd14.png"
+  }
+]);
